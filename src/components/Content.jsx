21eb0ed7fddef9d6835b02aef9e8bb81f1590392
Richard <richardlwhittington@gmail.com>
2025-03-25 00:03:19 -0500

    initial commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+const Content = () => {
+    return (
+      <div>
+        {/* <Part />
+        <Part />
+        <Part /> */}
+      </div>
+    )
+  }
+
+  export default Content;
---
+++ src/components/Total.jsx
@@ -0,0 +1,9 @@
+const Total = () => {
+    return (
+      <div>
+      
+      </div>
+    )
+  }
+
+  export default Total;
